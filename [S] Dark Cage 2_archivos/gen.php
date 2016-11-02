8cee199aac70e4a4073054ff64fe6ca0e0855ef7
khrigar <el.mundo.bizarro.de.ivan@gmail.com>
2016-11-02 14:47:55 -0300

    initial commit

---
+++ [S] Dark Cage 2_archivos/gen.php
@@ -0,0 +1,9 @@
+
+document.write("<table cellpadding=\"0\" cellspacing=\"0\" style=\"border-style:none;width:728px;background-color:#ffffff;\">");
+document.write("<tr style=\"background-color:#ffffff;\">");
+document.write("<td style=\"width:728px;background-color:#ffffff;vertical-align:top;text-align:center;\">");
+document.write("<a href=\"http://www.projectwonderful.com/out.php?go=173086352&_r=" +  escape(document.referrer) + "\" target=\"_blank\"><img src=\"http://www.projectwonderful.com/img/uploads/pics/44215-1460439224.gif\" width=728 height=90 border=0 title=\"Bittersweet Candy Bowl, a webcomic with a tsundere cat.\" alt=\"Bittersweet Candy Bowl, a webcomic with a tsundere cat.\"></a>");
+document.write("</td>");
+document.write("</tr>");
+document.write("<tr><td style=\"background-color:#ffffff;\" colspan=1><center><a style=\"font-size:10px;color:#737373;line-height:1.2;text-decoration:none;font-weight:bold;background-color:#ffffff;font-family:tahoma,verdana,arial,helvetica,sans-serif;text-transform: none;letter-spacing:normal;text-shadow:none;white-space:normal;word-spacing:normal;\" href=\"http://www.projectwonderful.com/advertisehere.php?id=78492&type=5&tag=42864\" target=\"_blank\">Ads by Project Wonderful!  Your ad could be here, right now.</a></center></td></tr>");
+document.write("</table>");
